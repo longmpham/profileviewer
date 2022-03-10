61e5a5069a4f90b536d69aecb3cd79d1e6567c42
Long <longish222@gmail.com>
2022-03-10 10:20:12 -0800

    added asynchandler for user api and added npm for jwt and bcrypt for user auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,17 @@
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
 const asyncHandler = require("express-async-handler");
 const UserModel = require("../models/user");
 
-const registerUser =  (req,res) => {
+const registerUser = asyncHandler(async (req,res) => {
   res.json({ message: "register user" })
-}
-const loginUser =  (req,res) => {
+})
+const loginUser = asyncHandler(async (req,res) => {
   res.json({ message: "login user" })
-}
-const getMe =  (req,res) => {
+})
+const getMe = asyncHandler(async (req,res) => {
   res.json({ message: "get user" })
-}
+})
 
 module.exports = {
   registerUser,
