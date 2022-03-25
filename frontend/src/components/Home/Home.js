861316ce343b02295988b936b26b686e031a8c67
Long <longish222@gmail.com>
2022-03-24 23:48:54 -0700

    added fake front page

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,8 +1,29 @@
 import React from 'react'
 
+import "./Home.css"
+
+import userData from "./data"
+
 const Home = () => {
+
+  const [users, setUsers] = React.useState(userData);
+  console.log(users)
+
   return (
-    <div>Home</div>
+    <div className="home-container">
+      {users.map(user => {
+        return (
+          <div className="home-profile-card" key={user.id}>
+            <img className="home-profile-image" src="" alt={`${user.name}'s profile picture`} />
+            <h1 className="home-profile-name" >{user.name}</h1>
+            <h1 className="home-profile-age" >{user.age}</h1>
+            <h1 className="home-profile-eyes" >{user.eyes}</h1>
+            <h1 className="home-profile-height" >{user.height}</h1>
+            <h1 className="home-profile-weight" >{user.weight}</h1>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
---
+++ frontend/src/components/Home/data.js
@@ -0,0 +1,36 @@
+const userData = [
+  {
+    "id": "1",
+    "name": "Bob",
+    "age": "10",
+    "eyes": "brown",
+    "height": "80",
+    "weight": "100",
+  },
+  {
+    "id": "2",
+    "name": "Joe",
+    "age": "20",
+    "eyes": "brown",
+    "height": "180",
+    "weight": "200",
+  },
+  {
+    "id": "3",
+    "name": "Hilary",
+    "age": "30",
+    "eyes": "brown",
+    "height": "60",
+    "weight": "80",
+  },
+  {
+    "id": "4",
+    "name": "Long",
+    "age": "40",
+    "eyes": "brown",
+    "height": "100",
+    "weight": "300",
+  },
+]
+
+export default userData;
